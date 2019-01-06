a3a5954b310b3854768d6563e1f7904d9fdcee70
herr kaste <herr.kaste@gmail.com>
2019-01-06 17:38:03 +0100

    Test AWAIT_WORKER timings on travis

--- tests/test_await_worker.py
+++ tests/test_await_worker.py
@@ -14,6 +14,7 @@ class TestAwaitingWorkerInDeferredTestCase(DeferrableTestCase):
 
     def test_ensure_plain_yield_is_faster_than_the_worker_thread(self):
         messages = []
+        TIME_FOR_WORK = 2
 
         def work(message, worktime=None):
             # simulate that a task might take some time
@@ -23,14 +24,22 @@ class TestAwaitingWorkerInDeferredTestCase(DeferrableTestCase):
 
             messages.append(message)
 
-        run_in_worker(work, 1, 5)
+        run_in_worker(work, 1, TIME_FOR_WORK)
 
+        start = time.time()
         yield
 
         self.assertEqual(messages, [])
 
+        yield lambda: len(messages) > 0
+        end = time.time()
+
+        print('awaiting took {}'.format(end - start))
+        self.assertTrue(end - start > TIME_FOR_WORK)
+
     def test_await_worker(self):
         messages = []
+        TIME_FOR_WORK = 1
 
         def work(message, worktime=None):
             # simulate that a task might take some time
@@ -40,8 +49,13 @@ class TestAwaitingWorkerInDeferredTestCase(DeferrableTestCase):
 
             messages.append(message)
 
-        run_in_worker(work, 1, 0.5)
+        run_in_worker(work, 1, TIME_FOR_WORK)
 
+        start = time.time()
         yield AWAIT_WORKER
+        end = time.time()
+
+        print('awaiting took {}'.format(end - start))
 
         self.assertEqual(messages, [1])
+        self.assertTrue(end - start > TIME_FOR_WORK)
